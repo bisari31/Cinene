41fd31501dbf1d0960e972af15938c42da6349bd
bisari <bisari31@gmail.com>
2022-11-10 20:56:10 +0900

    Feat: svg 추가

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,1 +1,5 @@
 export { ReactComponent as ChevronDown } from './chevronDown.svg';
+export { ReactComponent as New } from './new.svg';
+export { ReactComponent as Share } from './share.svg';
+export { ReactComponent as FolderAdd } from './folder-add.svg';
+export { ReactComponent as Upload } from './upload.svg';
