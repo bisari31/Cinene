ebe8168b2e3dc3acf01d8a3ded6d62b86ca46d7c
bisari <bisari31@gmail.com>
2022-11-10 21:03:10 +0900

    Feat: 디바운스 훅 추가

---
+++ client/src/hooks/useDebounce.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export default function useDebounce(text: string, timer: number) {
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    const executeFn = setTimeout(() => {
+      setValue(text);
+    }, timer);
+    return () => {
+      clearTimeout(executeFn);
+    };
+  }, [text, timer]);
+  return value;
+}
