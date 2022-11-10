4c463d9fb88e7df6b40c926cba2144b926a9b07d
bisari <bisari31@gmail.com>
2022-11-11 00:11:27 +0900

    Refactor: portal 컴포넌트 리팩토링

--- client/src/components/common/Modal.tsx
+++ client/src/components/common/Modal.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 interface IProps {
--- client/src/components/common/Portal.tsx
+++ client/src/components/common/Portal.tsx
@@ -1,10 +1,33 @@
+import { RefObject } from 'react';
 import ReactDOM from 'react-dom';
+import Modal from './Modal';
 
-interface IProps {
+interface PorTalProps {
   children: React.ReactNode;
 }
 
-export default function Portal({ children }: IProps) {
+interface IProps extends PorTalProps {
+  message: string;
+  visible: boolean;
+  refElement: RefObject<HTMLDivElement>;
+}
+
+function Portal({ children }: PorTalProps) {
   const modal = document.getElementById('portal') as HTMLElement;
   return ReactDOM.createPortal(children, modal);
+}
+
+export default function CustomPortal({
+  children,
+  message,
+  visible,
+  refElement,
+}: IProps) {
+  return (
+    <Portal>
+      <Modal message={message} visible={visible} refElement={refElement}>
+        {children}
+      </Modal>
+    </Portal>
+  );
 }
