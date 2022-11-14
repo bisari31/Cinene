a8ebac27b6afd364e724f81a3bf75fb7f36f332f
bisari <bisari31@gmail.com>
2022-11-14 23:30:08 +0900

    Docs: svg 추가

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as New } from './new.svg';
 export { ReactComponent as Share } from './share.svg';
 export { ReactComponent as FolderAdd } from './folder-add.svg';
 export { ReactComponent as Upload } from './upload.svg';
+export { ReactComponent as Edit } from './edit.svg';
