1d5e93165cd58d1615a6e97ebae9758e12b5f894
bisari <bisari31@gmail.com>
2022-11-24 17:19:47 +0900

    Design: svg 파일 추가

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,4 +1,6 @@
 export { ReactComponent as ChevronDown } from './chevronDown.svg';
+export { ReactComponent as ChevronLeft } from './chevronLeft.svg';
+export { ReactComponent as ChevronRight } from './chevronRight.svg';
 export { ReactComponent as New } from './new.svg';
 export { ReactComponent as Share } from './share.svg';
 export { ReactComponent as FolderAdd } from './folder-add.svg';
