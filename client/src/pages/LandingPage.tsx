e994ef4af7c18a2c06b26cae3c842554011dc3ed
bisari <bisari31@gmail.com>
2022-11-26 16:06:08 +0900

    Style: 컴포넌트 수정

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,16 +5,9 @@ import Popular from 'components/main/Popular';
 export default function LandingPage() {
   return (
     <LandingPageWrapper>
-      <h2>인기 작품</h2>
       <Popular />
     </LandingPageWrapper>
   );
 }
 
-const LandingPageWrapper = styled.div`
-  h2 {
-    font-size: 1.8rem;
-    font-weight: 500;
-    margin-bottom: 1em;
-  }
-`;
+const LandingPageWrapper = styled.div``;
