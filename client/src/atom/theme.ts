f7ffd40fb564e19b9728cd2d6a8abc81770c5cc0
bisari <bisari31@gmail.com>
2022-11-29 23:54:00 +0900

    Feat: 상태관리 추가

---
+++ client/src/atom/theme.ts
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const showsBackgroundState = atom({
+  key: 'showsBackgroundState',
+  default: false,
+});
+
+export const backdropUrlState = atom({
+  key: 'backdropUrlState',
+  default: '/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg',
+});
