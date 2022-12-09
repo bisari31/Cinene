617fc532712a288965ba84e15a3647336b9ea829
bisari <bisari31@gmail.com>
2022-12-09 22:52:24 +0900

    Feat: 이미지 모달 추가

---
+++ client/src/components/details/ModalImage.tsx
@@ -0,0 +1,96 @@
+import { Close } from 'assets';
+import useDarkOutside from 'hooks/useDarkOutside';
+import useEscapeClose from 'hooks/useEscapeClose';
+import { useEffect, useState } from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  src: string;
+  isVisible: boolean;
+  changeVisibility: () => void;
+  modalRef: React.RefObject<HTMLDivElement>;
+}
+
+export default function ModalImage({
+  src,
+  isVisible,
+  changeVisibility,
+  modalRef,
+}: Props) {
+  const [height, setHeight] = useState<number>();
+
+  useEscapeClose(isVisible, changeVisibility);
+  useDarkOutside(isVisible);
+
+  useEffect(() => {
+    setHeight(window.scrollY);
+  }, [isVisible]);
+
+  return (
+    <OutSide height={height}>
+      <Modal ref={modalRef}>
+        <div />
+        <img
+          src={
+            src ??
+            'https://blog.kakaocdn.net/dn/b8Kdun/btqCqM43uim/1sWJVkjEEy4LJMfR3mcqxK/img.jpg'
+          }
+          alt="poster"
+        />
+        <button type="button" onClick={() => changeVisibility()}>
+          <Close />
+        </button>
+      </Modal>
+    </OutSide>
+  );
+}
+const OutSide = styled.div<{ height: number | undefined }>`
+  ${({ height }) => css`
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.6);
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    left: 50%;
+    position: absolute;
+    top: ${`calc(${height}px + 50%)`};
+    transform: translate(-50%, -50%);
+    width: 100%;
+  `}
+`;
+
+const Modal = styled.div`
+  ${({ theme }) => css`
+    height: 70vh;
+    position: relative;
+    img {
+      height: 100%;
+      object-fit: contain;
+      width: 100%;
+    }
+    button {
+      align-items: center;
+      background: rgba(0, 0, 0, 0.4);
+      border: none;
+      border-radius: 3px;
+      display: flex;
+      height: 35px;
+      justify-content: center;
+      padding: 0;
+      position: absolute;
+      right: 1em;
+      top: 1em;
+      width: 35px;
+      svg {
+        fill: ${theme.colors.gray50};
+        line-height: 25px;
+        stroke: ${theme.colors.gray50};
+        stroke-width: 0.3;
+        width: 25px;
+      }
+    }
+    @media ${theme.device.tablet} {
+      height: 90vh;
+    }
+  `}
+`;
--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -1,76 +1,71 @@
-import { useEffect, useRef, useState } from 'react';
 import styled, { css } from 'styled-components';
 import { useQuery } from 'react-query';
 
 import { getMediaDetail, IMAGE_URL } from 'services/media';
-import { config } from 'styles/theme';
-import { numberRegex } from 'utils/regex';
 
 import Actors from 'components/details/Actors';
 import Description from 'components/details/Description';
 import useCurrentPathName from 'hooks/useCurrentPathName';
+import useOutsideClick from 'hooks/useOutsideClick';
+import Portal from 'components/common/Portal';
+import { useEffect, useState } from 'react';
+import ModalImage from 'components/details/ModalImage';
 
 export default function DetailPage() {
-  const [height, setHeight] = useState<number | undefined>();
-  const ref = useRef<HTMLDivElement>(null);
+  const { ref, isVisible, changeVisibility } = useOutsideClick();
+
   const { id, path } = useCurrentPathName();
+
   const { data } = useQuery([path, id], () => getMediaDetail(id, path));
 
-  useEffect(() => {
-    if (ref) {
-      if (ref.current && window.innerHeight < ref.current.offsetHeight) {
-        const HEADER = Number(config.header.match(numberRegex));
-        return setHeight(ref.current.scrollHeight + HEADER);
-      }
-      setHeight(window.innerHeight);
-    }
-  }, [ref]);
-
   return (
-    <DetailPageWrapper>
-      <Background
-        height={height}
-        src={
-          data?.backdrop_path
-            ? `${IMAGE_URL}/original/${data?.backdrop_path}`
-            : 'https://blog.kakaocdn.net/dn/b8Kdun/btqCqM43uim/1sWJVkjEEy4LJMfR3mcqxK/img.jpg'
-        }
-      >
-        <div />
-      </Background>
-      <Content ref={ref}>
+    <DetailPageWrapper
+      src={
+        data?.backdrop_path
+          ? `${IMAGE_URL}/original/${data?.backdrop_path}`
+          : 'https://blog.kakaocdn.net/dn/b8Kdun/btqCqM43uim/1sWJVkjEEy4LJMfR3mcqxK/img.jpg'
+      }
+    >
+      <div />
+      <Background />
+      <Content>
         <div>
-          <img
-            src={
-              data?.poster_path
-                ? `${IMAGE_URL}/original/${data?.poster_path}`
-                : 'https://blog.kakaocdn.net/dn/b8Kdun/btqCqM43uim/1sWJVkjEEy4LJMfR3mcqxK/img.jpg'
-            }
-            alt="poster"
-          />
+          <button type="button" onClick={() => changeVisibility()}>
+            <img
+              src={
+                data?.poster_path
+                  ? `${IMAGE_URL}/original/${data?.poster_path}`
+                  : 'https://blog.kakaocdn.net/dn/b8Kdun/btqCqM43uim/1sWJVkjEEy4LJMfR3mcqxK/img.jpg'
+              }
+              alt="poster"
+            />
+          </button>
         </div>
-        <Description data={data} path={path} id={id} />
-        <Actors path={path} id={id} />
+        <div>
+          <Description data={data} path={path} id={id} />
+          <Actors path={path} id={id} />
+        </div>
       </Content>
+      {isVisible && (
+        <Portal>
+          <ModalImage
+            modalRef={ref}
+            changeVisibility={changeVisibility}
+            isVisible={isVisible}
+            src={`${IMAGE_URL}/original/${data?.poster_path}`}
+          />
+        </Portal>
+      )}
     </DetailPageWrapper>
   );
 }
 
-const DetailPageWrapper = styled.div``;
-
-const Background = styled.div<{ src: string; height: number | undefined }>`
-  ${({ src, theme, height }) => css`
-    background-color: ${theme.colors.navy};
-    height: ${`${height}px`};
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 100%;
-    z-index: -1;
-    div {
+const DetailPageWrapper = styled.div<{ src: string }>`
+  ${({ src }) => css`
+    & > div:first-child {
       background: ${`linear-gradient(
-        rgba(24, 25, 32, 0.5) 10vh,
-        rgba(24, 25, 32, 1) 50vh
+      rgba(24, 25, 32, 0.5) 10vh,
+      rgba(24, 25, 32, 1) 50vh
       ), url(${src}) center`};
       background-size: cover;
       height: 50%;
@@ -78,7 +73,20 @@ const Background = styled.div<{ src: string; height: number | undefined }>`
       position: absolute;
       top: 0;
       width: 100%;
+      z-index: -1;
     }
+  `}
+`;
+
+const Background = styled.div`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.navy};
+    height: ${`calc(${theme.config.header} + 100% + 200px)`};
+    left: 0;
+    position: absolute;
+    top: ${theme.config.header};
+    width: 100%;
+    z-index: -2;
   `}
 `;
 
@@ -86,17 +94,42 @@ const Content = styled.article`
   ${({ theme }) => css`
     display: flex;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: space-around;
     padding-top: 15em;
     position: relative;
     & > div:nth-child(1) {
       bottom: 3em;
       position: relative;
-      img {
+      button {
+        background: none;
+        border: none;
         border-radius: 30px;
         height: 300px;
-        object-fit: cover;
+        overflow: hidden;
+        padding: 0;
         width: 200px;
+        img {
+          height: 100%;
+          object-fit: cover;
+          width: 100%;
+        }
+      }
+    }
+    & > div:last-child {
+      display: flex;
+      flex-direction: column;
+      div:last-child {
+      }
+    }
+
+    @media (max-width: 1050px) {
+      flex-direction: column;
+      }
+    }
+    @media (min-width: 1300px) {
+      justify-content: space-between;
+      & > div:last-child {
+        flex-direction: row;
       }
     }
   `}
