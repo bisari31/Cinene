ffc49609d9ad374e2c0f5358b5f9177a6e1d2bd9
bisari <bisari31@gmail.com>
2023-02-03 18:50:37 +0900

    Feat: usePrevious hook 추가

---
+++ client/src/hooks/usePrevious.ts
@@ -0,0 +1,15 @@
+import { useRef, useEffect } from 'react';
+
+export default function usePrevious<T>(value: T) {
+  const ref = useRef(value);
+
+  useEffect(() => {
+    ref.current = value;
+    console.log(
+      '🚀 ~ file: usePrevious.ts:9 ~ useEffect ~ ref.current',
+      ref.current,
+    );
+  });
+
+  return ref.current;
+}
