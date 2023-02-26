8b86f973da3301e50dd943763e66a724ed77b046
bisari <bisari31@gmail.com>
2023-02-26 22:17:51 +0900

    Refactor: 리뷰 api 리팩토링

--- server/src/models/review.ts
+++ server/src/models/review.ts
@@ -1,15 +1,18 @@
-import { model, ObjectId, Schema, Types } from 'mongoose';
+import { model, ObjectId, Schema, Types, Model } from 'mongoose';
+
+import Content from './content';
 
 export interface IReview {
+  _id: ObjectId;
   userId: ObjectId;
   contentId: ObjectId;
   contentType: string;
   comment: string;
   rating: number;
-  params?: { contentId: string; contentType: string };
-  isEditing: null | {
-    _id: string;
-  };
+}
+
+export interface ReviewModel extends Model<IReview> {
+  updateRatings: (contentId: any, contentType: any) => Promise<void>;
 }
 
 const reviewSchema = new Schema<IReview>(
@@ -35,5 +38,23 @@ const reviewSchema = new Schema<IReview>(
   { timestamps: true },
 );
 
-const Rating = model<IReview>('Review', reviewSchema);
+reviewSchema.statics.updateRatings = async function (
+  contentId: Pick<IReview, 'contentId'>,
+  contentType: Pick<IReview, 'contentType'>,
+) {
+  try {
+    const reviews: IReview[] = await this.find({ contentId, contentType });
+    const totalRating = reviews.reduce((acc, cur) => (acc += cur.rating), 0);
+    await Content.findOneAndUpdate(
+      { _id: contentId },
+      {
+        $set: { average: totalRating / reviews.length, votes: reviews.length },
+      },
+    );
+  } catch (err) {
+    throw { success: false, message: '평점 업데이트 에러 발생' };
+  }
+};
+
+const Rating = model<IReview, ReviewModel>('Review', reviewSchema);
 export default Rating;
