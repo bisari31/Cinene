1deaab282443c13803057629949f2b5210928ece
bisari <bisari31@gmail.com>
2023-03-09 20:18:36 +0900

    Style: 데이터 캐싱 시간 변경

--- client/src/hooks/queries/useCineneDataQuery.ts
+++ client/src/hooks/queries/useCineneDataQuery.ts
@@ -15,7 +15,7 @@ export default function useCineneDataQuery(
     cineneKeys.detail(type, id),
     () => getContent(type, id),
     {
-      staleTime: 1000 * 60 * 5,
+      staleTime: 1000 * 60 * 60,
       retry: false,
     },
   );
