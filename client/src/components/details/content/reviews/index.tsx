1c0b36a9b7acacd4bc3f7bd3fd064b2c02ca3b83
bisari <bisari31@gmail.com>
2023-03-26 03:12:18 +0900

    Style: 리뷰 컴포넌트 import path 변경

--- client/src/components/details/content/reviews/index.tsx
+++ client/src/components/details/content/reviews/index.tsx
@@ -10,7 +10,7 @@ import useAuthQuery from 'components/header/hooks/useAuthQuery';
 import Button from 'components/common/Button';
 import { useLoginPortal, useOutsideClick } from 'hooks';
 import ReviewList from './ReviewList';
-import ReviewModal from './reviewModal';
+import ReviewModal from './reviewModal/index';
 
 interface Props {
   data?: CineneData;
