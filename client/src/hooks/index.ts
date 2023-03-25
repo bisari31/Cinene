ecfd30833d7544f93fe9287b672b84ffe888e40b
bisari <bisari31@gmail.com>
2023-03-26 03:51:28 +0900

    Style: index 확장자명 변경

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -5,11 +5,11 @@ import useOutsideClick from './useOutsideClick';
 import usePreventScrolling from './usePreventScrolling';
 import usePrevious from './usePrevious';
 import useDebounce from './useDebounce';
-import useThrottle from './useThrottle';
 import useGetRelativeTime from './useRelativeTime';
 import useRedirection from './useRedirection';
 import useLoginPortal from './useLoginPortal';
 import useFocus from './useFocus';
+import useThrottle from './useThrottle';
 
 export {
   useRedirection,
