8e2e4aa283d4e621a9491e5b6013e039f8dada8d
bisari <bisari31@gmail.com>
2023-03-28 01:58:15 +0900

    Refactor: useFocus 훅 리팩토링

--- client/src/hooks/useFocus.tsx
+++ client/src/hooks/useFocus.tsx
@@ -1,7 +1,20 @@
-import { useEffect, RefObject } from 'react';
+import { RefObject, useCallback } from 'react';
 
-export default function useFocus<T extends HTMLElement>(ref: RefObject<T>) {
-  useEffect(() => {
-    ref.current?.focus();
-  }, [ref]);
+export default function useFocus<T>({ current }: RefObject<T>) {
+  const start = useCallback(() => {
+    if (current && current instanceof HTMLElement) current.focus();
+  }, [current]);
+
+  const end = useCallback(() => {
+    if (
+      current &&
+      (current instanceof HTMLInputElement ||
+        current instanceof HTMLTextAreaElement)
+    ) {
+      current.setSelectionRange(current.value.length, current.value.length);
+      current.focus();
+    }
+  }, [current]);
+
+  return { start, end };
 }
