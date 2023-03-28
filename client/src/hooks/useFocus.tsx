22ebbd315c29d278d6b19d8a0e56e9d46e2a0a78
bisari <bisari31@gmail.com>
2023-03-28 16:41:05 +0900

    Style: useFocus 타입 변경

--- client/src/hooks/useFocus.tsx
+++ client/src/hooks/useFocus.tsx
@@ -1,16 +1,14 @@
 import { RefObject, useCallback } from 'react';
 
-export default function useFocus<T>(ref: RefObject<T>) {
+export default function useFocus(
+  ref: RefObject<HTMLInputElement | HTMLTextAreaElement>,
+) {
   const start = useCallback(() => {
-    if (ref.current && ref.current instanceof HTMLElement) ref.current.focus();
+    if (ref.current) ref.current.focus();
   }, [ref]);
 
   const end = useCallback(() => {
-    if (
-      ref.current &&
-      (ref.current instanceof HTMLInputElement ||
-        ref.current instanceof HTMLTextAreaElement)
-    ) {
+    if (ref.current) {
       ref.current.setSelectionRange(
         ref.current.value.length,
         ref.current.value.length,
