d91c3ff10fd61ab9d844723fe1d8c9302274b3c7
bisari <bisari31@gmail.com>
2023-04-07 02:21:41 +0900

    Design: 로딩 스피너 css 변경

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -1,27 +1,34 @@
 import styled from 'styled-components';
 
 export default function Loading() {
-  return <StyledWrapper />;
+  return (
+    <StyledWrapper>
+      <div />
+    </StyledWrapper>
+  );
 }
 
 const StyledWrapper = styled.div`
-  animation: rotation 1s linear infinite;
-  border-radius: 50%;
-  border-right: 5px solid transparent;
-  border-top: 5px solid #dd3b5f;
-  box-sizing: border-box;
-  display: inline-block;
-  height: 78px;
   left: 50%;
   position: absolute;
   top: 40%;
-  width: 78px;
-  @keyframes rotation {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
+  transform: translate(-50%, -50%);
+  div {
+    animation: rotation 1s linear infinite;
+    border-radius: 50%;
+    border-right: 5px solid transparent;
+    border-top: 5px solid #dd3b5f;
+    box-sizing: border-box;
+    display: inline-block;
+    height: 78px;
+    width: 78px;
+    @keyframes rotation {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
     }
   }
 `;
